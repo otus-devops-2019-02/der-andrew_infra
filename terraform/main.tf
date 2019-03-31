ebbb95fcfece874c595ad210655152d87b06564d
Andrew <4andrey@rambler.ru>
2019-03-31 16:53:44 +0300

    Add resource - instance for GCP

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,22 @@ provider "google" {
 
   region = "europe-west-1"
 }
+
+resource "google_compute_instance" "app" {
+  name = "reddit-app"
+  machine_type = "g1-small"
+  zone = "europe-west1-d"
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "reddit-base"
+    }
+  }
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+}
