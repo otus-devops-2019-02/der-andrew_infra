32e722fc2bbe747f699c92a3a8556647d536d87c
Andrew <4andrey@rambler.ru>
2019-03-31 18:03:53 +0300

    Add resource firewall_puma

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,18 @@ resource "google_compute_instance" "app" {
     ssh-keys = "appuser:${file("~/.ssh/GCP-appuser.pub")}"
   }
 }
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports = ["9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
