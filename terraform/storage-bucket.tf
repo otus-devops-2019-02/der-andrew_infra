71f8acfba33bf5231af7181f6f750beb11041412
Andrew <4andrey@rambler.ru>
2019-04-08 21:49:55 +0300

    correct buckets names

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -9,7 +9,7 @@ module "storage-bucket" {
   version = "0.1.1"
 
   # Имена поменяйте на другие
-  name = ["storage-bucket-prod", "storage-bucket-stage"]
+  name = ["storage-bucket-product", "storage-bucket-stages"]
 }
 
 output storage-bucket_url {
