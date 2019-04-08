86c19581ec841194f8dd31b1982f778d6a1f7d09
Andrew <4andrey@rambler.ru>
2019-04-08 18:28:59 +0300

    load modules (terraform get)

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,17 @@ provider "google" {
 
   region = "${var.region}"
 }
+
+module "app" {
+  source = "modules/app"
+  public_key_path = "${var.public_key_path}"
+  zone = "${var.zone}"
+  app_disk_image = "${var.app_disk_image}"
+}
+
+module "db" {
+  source = "modules/db"
+  public_key_path = "${var.public_key_path}"
+  zone = "${var.zone}"
+  db_disk_image = "${var.db_disk_image}"
+}
