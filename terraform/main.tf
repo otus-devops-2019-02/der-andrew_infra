156f866af789894bde17f919077bac0919be5910
Andrew <4andrey@rambler.ru>
2019-04-08 19:09:29 +0300

    add parameter for vpc source_resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,4 +29,5 @@ module "db" {
 
 module "vpc" {
   source = "modules/vpc"
+  source_ranges = ["0.0.0.0/0"]
 }
--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -9,7 +9,7 @@ resource "google_compute_firewall" "firewall_ssh" {
     ports    = ["22"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = "${var.source_ranges}"
 }
 
 resource "google_compute_firewall" "firewall_puma" {
